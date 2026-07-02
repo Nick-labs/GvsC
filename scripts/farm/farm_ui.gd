5689928387ea4a8274e55646a66e2ce27c986b8f
Nick-labs <nikita.fomin.0404@ya.ru>
2026-07-02 11:40:16 +0300

    Add exit button

--- scripts/farm/farm_ui.gd
+++ scripts/farm/farm_ui.gd
@@ -4,11 +4,11 @@ extends CanvasLayer
 
 signal sell_pressed
 
-@onready var money_label: Label = $MoneyLabel
+@onready var money_label: Label = $MarginContainer/MoneyLabel
 
-@onready var name_label: Label = $BottomPanel/HBoxContainer/NameLabel
-@onready var price_label: Label = $BottomPanel/HBoxContainer/PriceLabel
-@onready var sell_button: Button = $BottomPanel/HBoxContainer/SellButton
+@onready var name_label: Label = $MarginContainer/BottomPanel/HBoxContainer/NameLabel
+@onready var price_label: Label = $MarginContainer/BottomPanel/HBoxContainer/PriceLabel
+@onready var sell_button: Button = $MarginContainer/BottomPanel/HBoxContainer/SellButton
 
 
 func show_pigeon(pigeon: Pigeon) -> void:
@@ -27,3 +27,7 @@ func _on_sell_button_pressed() -> void:
 
 func set_money(amount: int) -> void:
 	money_label.text = "Деньги: %d" % amount
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
