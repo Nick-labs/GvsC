42f99dd29e773f0ecee5f75e3ad3130ba235e5da
Nick-labs <nikita.fomin.0404@ya.ru>
2026-07-08 12:38:01 +0300

    Add HatchChance resource

--- scripts/farm/egg_data.gd
+++ scripts/farm/egg_data.gd
@@ -2,9 +2,12 @@ class_name EggData
 extends Resource
 
 @export var name: String
-@export var price: int = 3
+@export var price: int = 1
 @export var texture: Texture2D
 
 @export var parent_data: PigeonData
 
 @export var hatch_time_minutes := 3.0
+
+@export var tier: int
+@export var hatch_chances: Array[HatchChance]
---
+++ scripts/farm/hatch_chance.gd
@@ -0,0 +1,8 @@
+class_name HatchChance
+extends Resource
+
+@export_range(-4, 4)
+var tier_offset := 0
+
+@export_range(0.0, 100.0)
+var chance := 0.0
