849f54ff9a9075b288ddc75f4334d6be5b2eee16
Nick-labs <nikita.fomin.0404@ya.ru>
2026-07-10 08:09:22 +0300

    Add price panels

--- scripts/clock_display.gd
+++ scripts/clock_display.gd
@@ -1,9 +1,9 @@
 class_name ClockDisplay
 extends CanvasLayer
 
-@onready var day_label: Label = $MarginContainer/VBoxContainer/DayLabel
-@onready var time_label: Label = $MarginContainer/VBoxContainer/TimeLabel
-
+@onready var day_label: Label = $MarginContainer/Panel/MarginContainer/HBoxContainer/VBoxContainer/DayLabel
+@onready var time_label: Label = $MarginContainer/Panel/MarginContainer/HBoxContainer/VBoxContainer/TimeLabel
+@onready var money_label: Label = $MarginContainer/Panel/MarginContainer/HBoxContainer/MoneyLabel
 
 func _ready() -> void:
 	TimeManager.minute_passed.connect(_on_minute_passed)
@@ -22,3 +22,8 @@ func _on_minute_passed(day: int, hour: int, minute: int) -> void:
 func _update_labels() -> void:
 	day_label.text = "Day %d" % TimeManager.day
 	time_label.text = "%02d:%02d" % [TimeManager.hour, TimeManager.minute]
+	money_label.text = "%d грошей" % PlayerData.money
+
+
+func set_money(money: int):
+	money_label.text = "%d грошей" % money
